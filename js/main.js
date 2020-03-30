5c2d9cb7b603e95598ac217f50808de0fb4afc42
Andrea De Martino <ademartino1992@gmail.com>
2020-03-30 17:40:41 +0200

    Create: Style and Animation -- ONLY FOR IPHONE X

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@
 var emailList = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
 
 // Chiedo all’utente la sua email:
-var emailUser = prompt('Inserisci la tua email').toLowerCase();
+var emailUser = prompt('Inserisci la tua email');
 
 // Controllo che sia nella lista di chi può accedere e stampo un messaggio appropriato sull’esito dell controllo:
 for (var i = 0; i < emailList.length; i++){
-  if( emailList[i].toLowerCase() == emailUser){
+  if( emailList[i].toUpperCase() == emailUser.toUpperCase() ){
     var emailMatch = emailList[i];
     console.log('Email Trovata!');
+    document.getElementById('check-email').innerHTML = emailMatch.toUpperCase();
   }
 }
 
 if( emailMatch != emailUser){
   console.log('Email Non Trovata');
+  document.getElementById('check-email').innerHTML = 'EMAIL NON TROVATA';
 }
 
 // Gioco dei dadi
@@ -43,6 +45,14 @@ else if ( computerNumber == userNumber){
   console.log("Pareggio");
 }
 
+// INTERAZIONE BUTTON
+var playButton = document.getElementById('play-button');
+var manImg = document.getElementById('man-img');
 
-
+playButton.addEventListener('click', 
+  function() {
+    manImg.className = manImg.classList = 'man-moving';
+    console.log(manImg.classList);
+  }
+);
 
