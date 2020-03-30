cb90300beb76f3e55544fecd86dc0ee46a683e10
Andrea De Martino <ademartino1992@gmail.com>
2020-03-30 23:38:16 +0200

    Create: Dice Animation - ONLY FOR IPHONE X

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@ var emailList = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@bo
 
 // Chiedo all’utente la sua email:
 var emailUser = prompt('Inserisci la tua email');
-
+var emailCheck = false;
 // Controllo che sia nella lista di chi può accedere e stampo un messaggio appropriato sull’esito dell controllo:
 for (var i = 0; i < emailList.length; i++){
   if( emailList[i].toUpperCase() == emailUser.toUpperCase() ){
     var emailMatch = emailList[i];
     console.log('Email Trovata!');
+    alert('Accesso eseguito correttamente');
+    emailCheck = true;
     document.getElementById('check-email').innerHTML = emailMatch.toUpperCase();
   }
 }
@@ -16,6 +18,7 @@ for (var i = 0; i < emailList.length; i++){
 if( emailMatch != emailUser){
   console.log('Email Non Trovata');
   document.getElementById('check-email').innerHTML = 'EMAIL NON TROVATA';
+  alert('La tua email non è stata riconosciuta, non potrai giocare.');
 }
 
 // Gioco dei dadi
@@ -52,8 +55,11 @@ else if ( computerNumber == userNumber){
 // INTERAZIONE BUTTON
 var playButton = document.getElementById('play-button');
 var manImg = document.getElementById('man-img');
+var scorePlayer = document.getElementById('player-score');
+var scoreComputer =  document.getElementById('computer-score');
 var diceComputer;
 var dicePlayer;
+var resultShow = document.getElementById('result');
 
 if ((userNumber) == 1){
   dicePlayer = '<i class="fas fa-dice-one"></i>';
@@ -99,13 +105,17 @@ if (computerNumber == 6){
   diceComputer = '<i class="fas fa-dice-six"></i>';
 }
 
-playButton.addEventListener('click', 
+
+  playButton.addEventListener('click', 
   function() {
-    manImg.className = manImg.classList = 'man-moving';
+    if (emailCheck == true) {
+    manImg.classList = 'man-moving';
     console.log(manImg.classList);
-    document.getElementById('player-score').innerHTML = 'PLAYER SCORE:<br>'+ dicePlayer;
-    document.getElementById('computer-score').innerHTML = 'COMPUTER SCORE:<br>' + diceComputer;
+    resultShow.classList = 'u-green' + ' show';
+    scorePlayer.innerHTML = 'PLAYER SCORE:<br>'+ dicePlayer;
+    scoreComputer.innerHTML = 'COMPUTER SCORE:<br>' + diceComputer;
     document.getElementById('result').innerHTML = result;
   }
+}
 );
 
