69b7c358e5ed03a3dd6bdecc2d2b4012ac1a06b0
Andrea De Martino <ademartino1992@gmail.com>
2020-03-30 17:56:53 +0200

    Create: Style for result -- ONLY FOR IPONE X

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ if( emailMatch != emailUser){
 
 var userNumber;
 var computerNumber;
+var result;
 
 userNumber = (Math.floor((Math.random() * 6) + 1));
 console.log('Punteggio Giocatore ' + userNumber);
@@ -35,14 +36,17 @@ console.log('Punteggio Computer ' + computerNumber);
 // Stabilisco il vincitore, in base a chi fa il punteggio più alto:
 if( userNumber > computerNumber){
   console.log("Il giocatore vince");
+  result = 'IL GIOCATORE VINCE'
 }
 
 else if ( computerNumber > userNumber){
   console.log("Il Computer vince");
+  result = 'IL COMPUTER VINCE'
 }
 
 else if ( computerNumber == userNumber){
   console.log("Pareggio");
+  result = 'PAREGGIO'
 }
 
 // INTERAZIONE BUTTON
@@ -53,6 +57,11 @@ playButton.addEventListener('click',
   function() {
     manImg.className = manImg.classList = 'man-moving';
     console.log(manImg.classList);
+    document.getElementById('player-score').innerHTML = 'USER SCORE:<br>' + userNumber;
+    document.getElementById('computer-score').innerHTML = 'COMPUTER SCORE:<br>' + computerNumber;
+    document.getElementById('result').innerHTML = result;
+
+
   }
 );
 
